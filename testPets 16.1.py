f4abea9128f7424cad108a67c2313ebfbf4e2ee4
unknown <ktm1@mail.ru>
2023-01-17 17:18:37 +0600

    A new files added

---
+++ testPets 16.1.py
@@ -0,0 +1,17 @@
+from pets import Pets
+
+Cat = Pets("Барон", "мальчик", "2 года")
+
+print("Имеющиеся на сайте коты")
+print("Cat.name=", Cat.name)
+print("Cat.gender=", Cat.gender)
+print("Cat.age=", Cat.age)
+
+Cat2 = Pets("Сэм", "мальчик", "2 года")
+
+print("Cat.name=", Cat2.name)
+print("Cat.gender=", Cat2.gender)
+print("Cat.age=", Cat2.age)
+
+
+
